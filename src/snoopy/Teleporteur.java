61bbdf8b6f71289d83cb78519ff3a4d344aed46f
Unknown <benjamin.zamith@gmail.com>
2018-03-29 12:45:36 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/snoopy/Teleporteur.java
+++ src/snoopy/Teleporteur.java
@@ -1,10 +1,14 @@
 package snoopy;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 
-public class Teleporteur extends Objet {
+public class Teleporteur extends Objet implements Animation, Affichable {
     // Constantes
     public static final int RAYON = 20;
+    private int angle_rot=1;
 
     // Attributs
     private Teleporteur paire = null;
@@ -27,16 +31,20 @@ public class Teleporteur extends Objet {
 
     @Override
     public void afficher(Graphics2D g2d, Theme theme, int bx, int by) {
-        /*g2d.setColor(Color.black);
-        g2d.fillOval(
-                bx + (int) ((getX() + 0.5) * Moteur.LARG_IMG) - RAYON,
-                by + (int) ((getY() + 0.5) * Moteur.LONG_IMG) - RAYON,
-                2*RAYON, 2*RAYON
-        );*/
-        g2d.drawImage(theme.getPortailImg(),
-                bx + (int) ((getX() + 0.5) * Moteur.LARG_IMG) - RAYON,
-                by + (int) ((getY() + 0.5) * Moteur.LONG_IMG) - RAYON,
-                Moteur.LARG_IMG, Moteur.LONG_IMG,
+        BufferedImage img = theme.getPortailImg();
+
+        if(theme.getNumTheme() != Theme.SNOOPY)
+        {
+            angle_rot = (angle_rot + 1) % 24;
+            AffineTransform ma_rotation = AffineTransform.getRotateInstance(angle_rot*Math.PI/12, theme.getPortailImg().getWidth() / 2, theme.getPortailImg().getHeight() / 2);
+            AffineTransformOp op = new AffineTransformOp(ma_rotation, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+
+            img = op.filter(theme.getPortailImg(), null);
+        }
+        g2d.drawImage(img,
+                bx +  (getX()) * Moteur.LARG_IMG ,
+                by +  (getY() ) * Moteur.LONG_IMG,
+                //Moteur.LARG_IMG, Moteur.LONG_IMG,
                 null
         );
     }
@@ -52,5 +60,15 @@ public class Teleporteur extends Objet {
 
     public void setPaire(Teleporteur paire) {
         this.paire = paire;
+    }
+
+    @Override
+    public void animer(Carte carte, Theme theme) {
+
+    }
+
+    @Override
+    public boolean animation() {
+        return true;
     }
 }
